4b26057fe32066296d4db947c22caab2d21e2cb0
Fergus White <ferguswhite@outlook.com>
2022-12-26 16:35:08 +0000

    delete

--- not needed i think/index.js
+++
@@ -1,6 +0,0 @@
-export {default as Article} from './articles/Article'
-export {default as Brand} from './brand/Brand'
-export {default as CTA} from './cta/CTA'
-export {default as feature} from './feature/Feature'
-export {default as Navbar} from './navbar/Navbar'
-
